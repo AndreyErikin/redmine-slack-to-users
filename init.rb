4ab9a8176f2bc0696345740dd0f34e70505b0631
eliseev_aa <eliseevaa1989@gmail.com>
2014-12-26 11:21:22 +0800

    addd .gitignore

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require_dependency 'redmine_slack/listener'
 Redmine::Plugin.register :redmine_slack do
 	name 'Redmine Slack To User'
 	author 'com-k12'
-	url 'https://github.com/sciyoshi/redmine-slack'
+	url 'https://github.com/com-k12/redmine-slack'
 	author_url 'http://www.k12.ru'
 	description 'Slack chat integration'
 	version '0.1'
@@ -16,7 +16,7 @@ Redmine::Plugin.register :redmine_slack do
 		:default => {
 			'callback_url' => 'http://slack.com/callback/',
 			'channel' => nil,
-			'icon' => 'https://raw.github.com/com-k12/redmine-slack/gh-pages/icon.png',
+			'icon' => 'https://raw.github.com/sciyoshi/redmine-slack/gh-pages/icon.png',
 			'username' => 'redmine'
 		},
 		:partial => 'settings/slack_settings'
