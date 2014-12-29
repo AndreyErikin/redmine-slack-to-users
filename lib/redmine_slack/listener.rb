d40efb5edfc35ad73db41c0b8e186a67fc12943a
eliseev_aa <eliseevaa1989@gmail.com>
2014-12-29 12:56:45 +0800

    add bugs

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -62,7 +62,7 @@ class SlackListener < Redmine::Hook::Listener
 		watchers2 = journal.watcher_recipients
 		puts "watchers", watchers1, watchers2
 		puts ".find_by_mail ", User.find_by_mail(watchers1[0])
-		puts ".find_by_mail ", UserCustomField.find_by_login(watchers1[1], "Slack User")
+		puts ".find_by_mail ", User.find_by_mail(watchers1[1]).custom_value_for(1)
 
 		channel = channel_for_project issue.project
 		url = url_for_project issue.project
